99f89a3fc75e09a456d53008410425a02da48926
victorhanert <vict660m@stud.kea.dk>
2024-02-11 18:46:18 +0100

    First server

--- 02._FirstServer/app.js
+++ 02._FirstServer/app.js
@@ -15,10 +15,27 @@ app.get("/secondRoute", (req, res) => {
 
 app.get("/thirdRoute/:someValue/:someOtherValue", (req, res) => {
     const firstValue = req.params.someValue;
-    const secondValue = req.params.someOtherValue
+    const secondValue = req.params.someOtherValue;
     console.log(firstValue, secondValue);
     console.log(req.params);
     res.send({ data: "this is the thirdroute"});
+});
+
+let balance = 100;
+app.get("/wallet/:withdrawalAmount", (req, res) => {
+    /* start with the balance of 100 in the wallet
+        every time a client calls this route the value of paymentOut should be subtracted from the balance
+        if the paymentOut creates a balance below 0 then tell the client "Sorry, not enough funds" */
+        const withdrawalAmount = Number(req.params.withdrawalAmount);
+        
+    if (!withdrawalAmount) res.send({ data: "You have submitted an incorrect amount, (not a number)." });
+
+    if (balance - withdrawalAmount < 0) {
+        res.send({ data: "Sorry, not enough funds"});
+    } else {
+        balance -= withdrawalAmount;
+        res.send({ data: balance });
+    }
 });
 
 
