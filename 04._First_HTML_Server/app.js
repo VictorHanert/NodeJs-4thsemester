b90ec708e6a59ae27109e8bd0ba5a28924511c9b
victorhanert <vict660m@stud.kea.dk>
2024-02-20 19:19:49 +0100

    changes to html-server

--- 04._First_HTML_Server/app.js
+++ 04._First_HTML_Server/app.js
@@ -2,6 +2,13 @@ const express = require("express");
 
 const app = express();
 
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/homepage.html");
+});
+
+app.get("/publicsquare", (req, res) => {
+    res.sendFile(__dirname + "/publicSquare.html");
+});
 
 
 
