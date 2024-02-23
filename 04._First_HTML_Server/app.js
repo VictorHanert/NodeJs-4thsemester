981f66d5bf9228ade9829751f073f7b87cc819ab
victorhanert <vict660m@stud.kea.dk>
2024-02-23 17:22:42 +0100

    practiced with fetching html pages

--- 04._First_HTML_Server/app.js
+++ 04._First_HTML_Server/app.js
@@ -3,14 +3,26 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/homepage.html");
+    res.sendFile(__dirname + "/public/homepage/homepage.html");
 });
 
 app.get("/publicsquare", (req, res) => {
-    res.sendFile(__dirname + "/publicSquare.html");
+    res.sendFile(__dirname + "/public/publicSquare/publicSquare.html");
 });
 
+app.get("/greeting", (req, res) => {
+    if (req.query.name) {
+        res.send({ "data": `Hello, ${req.query.name}! Welcome to the homepage` });
+    }
+    else {
+        res.send({ "data": "Hello Stranger, who are you?" });
+    }
+});
 
+const knownNames = ["John", "Jim", "Bob"];
+app.get("/knownpeople", (req, res) => {
+    res.send({ "data": knownNames.length });
+});
 
 // ---------- bottom ------------
 const PORT = 8080;
