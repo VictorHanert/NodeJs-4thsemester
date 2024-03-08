7f1354efeac0cb8ccbe390b958efb42abfb538ee
victorhanert <vict660m@stud.kea.dk>
2024-03-08 11:09:21 +0100

    updated dogInder for SSR

--- 07._DogInder/app.js
+++ 07._DogInder/app.js
@@ -5,21 +5,35 @@ app.use(express.static('public'));
 
 import path from 'path';
 import getMatches from './util/matches.js';
+import fs from "fs";
 
-getMatches();
+const header = fs.readFileSync("./public/components/header/header.html").toString();
+const footer = fs.readFileSync("./public/components/footer/footer.html").toString();
+const homepage = fs.readFileSync("./public/pages/homepage/homepage.html").toString();
+const matches = fs.readFileSync("./public/pages/matches/matches.html").toString();
+const contact = fs.readFileSync("./public/pages/contact/contact.html").toString();
+
+
+const homepagePage = header + homepage + footer;
+const matchesPage = header + matches + footer;
+const contactPage = header + contact + footer;
 
 
 // =========== HTML ============
 app.get("/", (req, res) => {
-    res.sendFile(path.resolve("public/pages/homepage/homepage.html"));
+    res.send(homepagePage);
 });
 
 app.get("/matches", (req, res) => {
-    res.sendFile(path.resolve("public/pages/matches/matches.html"));
+    res.send(matchesPage);
 });
 
 app.get("/contact", (req, res) => {
-    res.sendFile(path.resolve("public/pages/contact/contact.html"));
+    res.send(contactPage);
+});
+
+app.get("/page", (req, res) => {
+    res.send("<div><h1>This is a page</h1><p>Some information</p></div>")
 });
 
 // =========== API ============
