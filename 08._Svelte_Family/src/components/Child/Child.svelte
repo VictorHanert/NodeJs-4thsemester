5b7e8e2ea6aa6ffc07de0c2f530aa5f89ff005e6
victorhanert <vict660m@stud.kea.dk>
2024-04-05 11:18:55 +0200

    fridgemessages

--- 08._Svelte_Family/src/components/Child/Child.svelte
+++ 08._Svelte_Family/src/components/Child/Child.svelte
@@ -1,35 +1,54 @@
 <script>
-    export let child;
-    export let onShowLove;
-    export let onTakeFromTreasureChest;
+  export let child;
+  export let onShowLove;
+  export let onTakeFromTreasureChest;
+
+  import { fridgeMessages } from "../../stores/fridgeMessageStore";
+
+  let fridgeMessageInputValue = "";
+
+  function submitFridgeMessage() {
+    const newFridgeMessage = {
+        creator: child.name,
+        message: fridgeMessageInputValue
+    };
+    fridgeMessages.set([... $fridgeMessages, newFridgeMessage]);
+    fridgeMessageInputValue = "";
+  }
 </script>
 
 <div
-    class:is-girl={child.isGirl}
-    class:is-boy={!child.isGirl}
-    class={child.familySheep || "not-a-black-sheep"}
+  class:is-girl={child.isGirl}
+  class:is-boy={!child.isGirl}
+  class={child.familySheep || "not-a-black-sheep"}
 >
-    <p>{child.name}</p>
-    <button on:click={() => onShowLove(child.name)}>Show Love 💚</button>
-    <br>
-    <button on:click={onTakeFromTreasureChest}>Take from treasure chest</button>
+  <p>{child.name}</p>
 
+  <label for="fridgeMessage">Write a fridge message</label>
+  <input type="text" name="fridgeMessage" placeholder="Write message" 
+  bind:value={fridgeMessageInputValue}/>
+  <br />
+  <button on:click={submitFridgeMessage}>Post fridge message</button>
+  <br />
+  <button on:click={() => onShowLove(child.name)}>Show Love 💚</button>
+  <br />
+  <button on:click={onTakeFromTreasureChest}>Take from treasure chest</button>
 </div>
 
 <style>
-    .is-girl {
-        background-color: #ff3dd8;
-    }
-    .is-boy {
-        background-color: #39bdff;
-    }
-    .black-sheep {
-        border: 5px solid black;
-    }
-    .grey-sheep {
-        border: 5px solid #d5d5d5;
-    }
-    .not-a-black-sheep {
-        border: 5px solid white;
-    }
+  .is-girl {
+    background-color: #ff3dd8;
+  }
+  .is-boy {
+    background-color: #39bdff;
+  }
+  .black-sheep {
+    border: 5px solid black;
+  }
+  .grey-sheep {
+    border: 5px solid #d5d5d5;
+  }
+  .not-a-black-sheep {
+    border: 5px solid white;
+  }
 </style>
--- 08._Svelte_Family/src/components/Home/Home.svelte
+++ 08._Svelte_Family/src/components/Home/Home.svelte
@@ -1,5 +1,24 @@
-<h1 style="border: 1px solid black; padding: 20px;">
+<script>
+    import { fridgeMessages } from "../../stores/fridgeMessageStore";
+</script>
+
+<h2 style="border: 1px solid black; padding: 20px;">
     Home
-</h1>
+</h2>
+
+<div class="fridge">
+    {#each $fridgeMessages as fridgeMessage}
+    <p>{fridgeMessage.creator ?? "Title"}: {fridgeMessage.message}</p>
+    {/each}
+</div>
 
 <slot></slot>
+
+<style>
+    .fridge {
+        background-color: aliceblue;
+        padding: 200px 80px;
+        border-radius: 20px;
+        border: 1px solid black;
+    }
+</style>
