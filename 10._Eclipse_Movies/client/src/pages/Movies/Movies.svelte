3d94a6b721d7819c9a008ccdd9260dca97e3e6e3
victorhanert <vict660m@stud.kea.dk>
2024-04-19 09:41:34 +0200

    fixed CORS

--- 10._Eclipse_Movies/client/src/pages/Movies/Movies.svelte
+++ 10._Eclipse_Movies/client/src/pages/Movies/Movies.svelte
@@ -1,1 +1,21 @@
+<script>
+    import { onMount } from "svelte";
+    import { BASE_URL } from "../../stores/generalStore";
+
+    let movies = [];
+
+    onMount(() => {
+        fetch($BASE_URL + "/api/movies")
+        .then((response) => response.json())
+        .then((result) => {
+            movies = result.data
+        });
+    });
+</script>
+
 <h1>Movies</h1>
+
+{#each movies as movie}
+ <p>{movie.id}</p>
+ <h3>{movie.title} - by {movie.director} - from {movie.year}</h3>
+ {/each}
--- 10._Eclipse_Movies/server/app.js
+++ 10._Eclipse_Movies/server/app.js
@@ -1,6 +1,12 @@
 import express from 'express';
 const app = express();
 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 import moviesRouter from "./routers/movieRouters.js";
 app.use(moviesRouter);
 
