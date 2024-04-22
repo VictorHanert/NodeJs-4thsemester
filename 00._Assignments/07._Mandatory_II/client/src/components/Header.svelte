2fc994caff824bc8e0c29ccf1ad9099f1aecd962
victorhanert <vict660m@stud.kea.dk>
2024-04-22 22:15:20 +0200

    added more stuff

--- 00._Assignments/07._Mandatory_II/client/src/components/Header.svelte
+++ 00._Assignments/07._Mandatory_II/client/src/components/Header.svelte
@@ -17,20 +17,21 @@
       : " py-6 bg-transparent border-transparent")}
 >
   <h1 class="font-medium">
-    <a href="/myspace" class="hover:text-violet-400 duration-300 hover:text-2xl">
+    <a
+      href="/myspace"
+      class="hover:text-violet-400 duration-300 hover:text-2xl"
+    >
       <b class="font-bold poppins">My</b> Space
     </a>
   </h1>
   <div class="sm:flex items-center gap-4 hidden">
-    {#each tabs as tab, index}
-      <a
-        href={tab.link}
-        class="duration-200 hover:text-violet-400"
-        target={index === 2 ? "_blank" : ""}
-      >
-        <p>{tab.name}</p>
-      </a>
-    {/each}
+    <a href="/" class="duration-200 hover:text-violet-400">
+      <p>Home <i class="fa-solid fa-home"></i></p>
+    </a>
+
+    <a href="/about" class="duration-200 hover:text-violet-400">
+      <p>About <i class="fa-solid fa-bars-staggered"></i></p>
+    </a>
 
     {#if isLoggedIn === false}
       <a
@@ -40,11 +41,16 @@
         <div
           class="absolute top-0 right-full w-full h-full bg-violet-400 opacity-20 group-hover:translate-x-full z-0 duration-200"
         />
-        <h4 class="relative z-9">Login &rarr;</h4>
+        <h4 class="relative z-9">
+          Login <i class="fa-solid fa-right-to-bracket"></i>
+        </h4>
       </a>
     {:else}
-      <button class="blueShadow px-5 py-2 rounded-full bg-violet-400 hover:bg-violet-500"
-      on:click={logout}>Logout ⎋</button>
+      <button
+        class="blueShadow px-5 py-2 rounded-full bg-violet-400 hover:bg-violet-500"
+        on:click={logout}
+        >Logout <i class="fa-solid fa-right-from-bracket"></i></button
+      >
     {/if}
   </div>
 </header>
--- 00._Assignments/07._Mandatory_II/client/src/routes/+page.svelte
+++ 00._Assignments/07._Mandatory_II/client/src/routes/+page.svelte
@@ -2,7 +2,7 @@
     import Main from "../components/Main.svelte";
 </script>
 <svelte:head>
- <title>My Space</title>
+ <title>MySpace</title>
 </svelte:head>
 
 <Main />
