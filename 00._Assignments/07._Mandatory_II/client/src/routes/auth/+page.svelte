59eba390e65e71f970bce6f426987e9ea6bcae7b
victorhanert <vict660m@stud.kea.dk>
2024-04-23 16:58:16 +0200

    Refactored files

--- 00._Assignments/07._Mandatory_II/client/src/routes/auth/+page.svelte
+++ 00._Assignments/07._Mandatory_II/client/src/routes/auth/+page.svelte
@@ -1,7 +1,7 @@
 <script>
     import { slide } from 'svelte/transition';
-    import Login from "$lib/components/Login/Login.svelte";
-    import Signup from "$lib/components/Signup/Signup.svelte";
+    import LoginForm from "$lib/components/LoginForm/LoginForm.svelte";
+    import SignupForm from "$lib/components/SignupForm/SignupForm.svelte";
   
     let activeForm = "login";
   </script>
@@ -11,13 +11,13 @@
       <div class="w-full max-w-md bg-white p-8 rounded shadow-md min-h-form">
         {#if activeForm === "login"}
         <div class="front" transition:slide={{ delay: 250, duration: 200}}>
-          <Login />
+          <LoginForm />
           <p class="text-center mt-5">Don't have an account? 
             <button on:click={() => (activeForm = "signup")} class="text-blue-500 underline">Sign up</button></p>
         </div>
         {:else if activeForm === "signup"}
         <div class="back" transition:slide={{ delay: 250, duration: 200}}>
-          <Signup />
+          <SignupForm />
           <p class="text-center mt-5">Already have an account?
             <button on:click={() => (activeForm = "login")} class="text-blue-500 underline">Login</button></p>
         </div>
