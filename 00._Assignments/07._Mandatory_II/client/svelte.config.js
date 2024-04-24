904e26359a661d0daec58e6bab7722c3fbf6b7a2
victorhanert <vict660m@stud.kea.dk>
2024-04-24 09:40:27 +0200

    updates svelte config for trying Vercel

--- 00._Assignments/07._Mandatory_II/client/svelte.config.js
+++ 00._Assignments/07._Mandatory_II/client/svelte.config.js
@@ -1,10 +1,15 @@
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
     adapter: adapter()
   },
-  preprocess: vitePreprocess()
+  preprocess: vitePreprocess(),
+  // Specify the Node.js version
+  // Add this line to your configuration
+  target: 'node@16.0.0' // or 'node@18.0.0'
 };
+
 export default config;
