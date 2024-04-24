491f4d8eeaa9a8821d0491268e5dc04d8dfa30df
victorhanert <vict660m@stud.kea.dk>
2024-04-24 10:10:00 +0200

    vercel again

--- 00._Assignments/07._Mandatory_II/client/svelte.config.js
+++ 00._Assignments/07._Mandatory_II/client/svelte.config.js
@@ -1,15 +1,10 @@
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
     adapter: adapter()
   },
-  preprocess: vitePreprocess(),
-  // Specify the Node.js version
-  // Add this line to your configuration
-  target: 'node@16.0.0' // or 'node@18.0.0'
+  preprocess: vitePreprocess()
 };
-
 export default config;
